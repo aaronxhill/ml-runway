74c6bff489a378ecf25d81526fd452415a6abe3e
Ubuntu <ubuntu@ip-172-31-58-108.ec2.internal>
2017-05-22 00:30:18 +0000

    latest version of app

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var request = require('request');
 var webshot = require('webshot');
 var AWS = require('aws-sdk');
 var winston = require('winston');
+var mailcomposer = require('mailcomposer');
+
+//var serverStart = new Date();
+//var cellNumbers = ["+19172541441", "+16462984753"];
 
 // OPTIONS
 
@@ -44,6 +48,7 @@ function processResponse(error, response, body) {
             var s3bucket = new AWS.S3({
                 params: {
                     Bucket: 'ml-runway'
+		    //StartAfter: '2016-10-01T22:08:08_560Z'
                 }
             });
 
@@ -62,15 +67,16 @@ function processResponse(error, response, body) {
                     else {
                         winston.info("uploaded new image: ", info.last_event.start_time);
                         saveIndex();
-                    }
+			// AI AND EMAIL AND MMS
+			getLabels(info.last_event.start_time.replace('.', '_'))
+			//if ((new Date() - serverStart) > 120000) {
+			//	sendMms(info.last_event.start_time.replace('.', '_'))
+                   // }
                 });
             });
 
 
         }
-        // else {winston.info('HA CHA! Response status code: ', response.statusCode, 'Error: ', error);}
-
-
     }
     else {
         winston.info('NOT LOGGED! Response status code: ', response.statusCode, 'Error: ', error);
@@ -80,7 +86,8 @@ function processResponse(error, response, body) {
 // VIEWER IN S3
 function saveIndex() {
     var params = {
-        Bucket: 'ml-runway' /* required */
+        Bucket: 'ml-runway', /* required */
+	StartAfter: '2017-04-27T16:39:00_960Z'
             //   ContinuationToken: 'STRING_VALUE',
             //   Delimiter: 'STRING_VALUE',
             //   EncodingType: 'url',
@@ -125,6 +132,115 @@ function saveIndex() {
     });
 
 }
+
+// REKOGNITION AND EMAIL
+function getLabels(imageName) {
+
+    var rekognition = new AWS.Rekognition({
+        region: 'us-east-1'
+    });
+
+    var params = {
+        Image: {
+            S3Object: {
+                Bucket: "ml-runway",
+                Name: imageName
+            }
+        },
+        MaxLabels: 123,
+        MinConfidence: 55
+    };
+    rekognition.detectLabels(params, function(err, data) {
+        if (err) winston.info(err, err.stack); // an error occurred
+        else {
+            // plane logic here
+            var hasPlane = false;
+            for (var i = 0; i < data.Labels.length; i++) {
+                if (data.Labels[i].Name == 'Airplane') {
+                    hasPlane = true
+                }
+                else if (data.Labels[i].Name == 'Flying') {
+                    hasPlane = true
+                }
+            }
+            if (hasPlane == true) {
+                emailPlane(imageName)
+            }
+            else {
+                emailNoPlane(imageName)
+            }
+
+        }; // successful response
+    });
+}
+
+function emailPlane(foto) {
+    var api_key = process.env.MAILGUN_KEY;
+    var domain = 'sandbox47a06ea6aea54b5e9a408e3c2de6a8b7.mailgun.org';
+    var mailgun = require('mailgun-js')({
+        apiKey: api_key,
+        domain: domain
+    });
+
+    var mail = mailcomposer({
+        from: 'Do Not Reply <postmaster@sandbox47a06ea6aea54b5e9a408e3c2de6a8b7.mailgun.org>',
+        to: 'aaron.hill@me.com',
+        subject: 'Plane at Flying Dollar Airport',
+        text: 'A plane at Flying Dollar Airport! See the image at http://ml-runway.s3-website-us-east-1.amazonaws.com/' + foto,
+        html: '<h3>There is a plane at Flying Dollar Airport!</h3><p><img src="http://ml-runway.s3-website-us-east-1.amazonaws.com/' + foto + '"></img></p>'
+    });
+
+    mail.build(function(mailBuildError, message) {
+
+        var dataToSend = {
+            to: 'aaron.hill@me.com',
+            message: message.toString('ascii')
+        };
+
+        mailgun.messages().sendMime(dataToSend, function(sendError, body) {
+            if (sendError) {
+                winston.info(sendError);
+                return;
+            }
+        });
+    });
+
+}
+
+function emailNoPlane(foto) {
+    var api_key = process.env.MAILGUN_KEY;
+    var domain = 'sandbox47a06ea6aea54b5e9a408e3c2de6a8b7.mailgun.org';
+    var mailgun = require('mailgun-js')({
+        apiKey: api_key,
+        domain: domain
+    });
+
+    var mail = mailcomposer({
+        from: 'Do Not Reply <postmaster@sandbox47a06ea6aea54b5e9a408e3c2de6a8b7.mailgun.org>',
+        to: 'aaron.hill@me.com',
+        subject: 'Runway Cam Activity',
+        text: 'The runway cam detected activity, but it is not an airplane. See the image at http://ml-runway.s3-website-us-east-1.amazonaws.com/' + foto,
+        html: '<h3>The runway cam detected activity, but it is not an airplane.</h3><p><img src="http://ml-runway.s3-website-us-east-1.amazonaws.com/' + foto + '"></img></p>'
+    });
+
+    mail.build(function(mailBuildError, message) {
+
+        var dataToSend = {
+            to: 'aaron.hill@me.com',
+            message: message.toString('ascii')
+        };
+
+        mailgun.messages().sendMime(dataToSend, function(sendError, body) {
+            if (sendError) {
+                winston.info(sendError);
+                return;
+            }
+        });
+    });
+}
+
+// TWILIO
+
 
 // ITERATE
 
