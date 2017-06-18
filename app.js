dc790ad59eb0ab6443d03ce42205240a77a8e568
Aaron Hill <aaron.hill@me.com>
2017-06-18 07:53:42 -0400

    first attempt at new emails

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ var mailcomposer = require('mailcomposer');
 
 var serverStart = new Date();
 
-//var cellNumbers = ["+19172541441", "+16462984753", "+16015068935", "+19737148364"];
-var cellNumbers = ["+19172541441", "+16462984753"];
+var cellNumbers = JSON.parse(fs.readFileSync('cells.txt'));
+
+var comm = require('./communication');
 
 // OPTIONS
 
@@ -50,7 +51,6 @@ function processResponse(error, response, body) {
             var s3bucket = new AWS.S3({
                 params: {
                     Bucket: 'ml-runway'
-		    //StartAfter: '2016-10-01T22:08:08_560Z'
                 }
             });
 
@@ -68,10 +68,9 @@ function processResponse(error, response, body) {
                     }
                     else {
                         winston.info("uploaded new image: ", info.last_event.start_time);
-                        saveIndex();
+                        // saveIndex();
 			// AI AND EMAIL AND MMS
 			if ((new Date() - serverStart) > 120000) {
-				//sendMms(info.last_event.start_time.replace('.', '_'))
 				getLabels(info.last_event.start_time.replace('.', '_'))
                     }
                 }});
@@ -85,55 +84,47 @@ function processResponse(error, response, body) {
     }
 }
 
-// VIEWER IN S3
-function saveIndex() {
-    var params = {
-        Bucket: 'ml-runway', /* required */
-	StartAfter: '2017-04-27T16:39:00_960Z'
-            //   ContinuationToken: 'STRING_VALUE',
-            //   Delimiter: 'STRING_VALUE',
-            //   EncodingType: 'url',
-            //   FetchOwner: true || false,
-            //   MaxKeys: 10
-            //   Prefix: 'STRING_VALUE',
-            //   StartAfter: 'STRING_VALUE'
-    };
+// // VIEWER IN S3
+// function saveIndex() {
+//     var params = {
+//         Bucket: 'ml-runway', /* required */
+// 	StartAfter: '2017-04-27T16:39:00_960Z'
+//     };
     
-                var s3bucket = new AWS.S3({
-                params: {
-                    Bucket: 'ml-runway'
-                }
-            });
+//                 var s3bucket = new AWS.S3({
+//                 params: {
+//                     Bucket: 'ml-runway'
+//                 }
+//             });
 
-    s3bucket.listObjectsV2(params, function(err, data) {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            var toWrite = '<!doctype html><html><head><title>S3 F$ Runway Photos (20)</title><meta name="F$ runway photos in S3; 20 most recent" content="S3"><meta charset="utf-8"></head><body>';
-            // for (var i = 1; i < data.Contents.length + 1; i++) {
-            for (var i = 1; i < 22; i++) {
-                if (data.Contents[data.Contents.length - i].Key != 'index.html') {
-                    toWrite = toWrite + "<img src='http://ml-runway.s3-website-us-east-1.amazonaws.com/" + data.Contents[data.Contents.length - i].Key + "'/>";
-                }
-            }
-        }
-        toWrite = toWrite + "</body></html>";
-        s3bucket.upload({
-            Key: 'index.html',
-            Body: toWrite,
-            ContentType: 'text/html'
-        }, function(err, data) {
-            if (err) {
-                console.log(err);
-            }
-            else {
-                // winston.info("uploaded new image: ", info.last_event.start_time);
-            }
-        });
-    });
+//     s3bucket.listObjectsV2(params, function(err, data) {
+//         if (err) {
+//             console.log(err);
+//         }
+//         else {
+//             var toWrite = '<!doctype html><html><head><title>S3 F$ Runway Photos (20)</title><meta name="F$ runway photos in S3; 20 most recent" content="S3"><meta charset="utf-8"></head><body>';
+//             for (var i = 1; i < 22; i++) {
+//                 if (data.Contents[data.Contents.length - i].Key != 'index.html') {
+//                     toWrite = toWrite + "<img src='http://ml-runway.s3-website-us-east-1.amazonaws.com/" + data.Contents[data.Contents.length - i].Key + "'/>";
+//                 }
+//             }
+//         }
+//         toWrite = toWrite + "</body></html>";
+//         s3bucket.upload({
+//             Key: 'index.html',
+//             Body: toWrite,
+//             ContentType: 'text/html'
+//         }, function(err, data) {
+//             if (err) {
+//                 console.log(err);
+//             }
+//             else {
+//                 // winston.info("uploaded new image: ", info.last_event.start_time);
+//             }
+//         });
+//     });
 
-}
+// }
 
 // REKOGNITION AND EMAIL
 function getLabels(imageName) {
@@ -150,114 +141,90 @@ function getLabels(imageName) {
             }
         },
         MaxLabels: 123,
-        MinConfidence: 60
+        MinConfidence: 30
     };
     rekognition.detectLabels(params, function(err, data) {
         if (err) winston.info(err, err.stack); // an error occurred
         else {
             // plane logic here
             var hasPlane = false;
+            var maybePlane = false; 
             for (var i = 0; i < data.Labels.length; i++) {
                 if (data.Labels[i].Name == 'Airplane') {
-                    hasPlane = true
+                    if (data.Labels[i].Confidence >= 60) {
+                        hasPlane = true
+                    }
+                    else {maybePlane = true}
                 }
-                else if (data.Labels[i].Name == 'Flying') {
-                    hasPlane = true
-                }
+                // else if (data.Labels[i].Name == 'Flying') {
+                    // hasPlane = true
+                // }
             }
             if (hasPlane == true) {
-                emailPlane(imageName)
-		sendMms(imageName)
+                comm.emailPlane(imageName, true);
+                // emailPlane(imageName)
+		        comm.sendMms(imageName)
+            }
+            else if (maybePlane == true) {
+                comm.emailPlane(imageName, null);
             }
             else {
-                emailNoPlane(imageName)
+                comm.emailPlane(imageName, false);
             }
 
         }; // successful response
     });
 }
 
-function emailPlane(foto) {
-    var api_key = process.env.MAILGUN_KEY;
-    var domain = 'sandbox47a06ea6aea54b5e9a408e3c2de6a8b7.mailgun.org';
-    var mailgun = require('mailgun-js')({
-        apiKey: api_key,
-        domain: domain
-    });
+// function emailPlane(foto) {
+//     var api_key = process.env.MAILGUN_KEY;
+//     var domain = 'sandbox47a06ea6aea54b5e9a408e3c2de6a8b7.mailgun.org';
+//     var mailgun = require('mailgun-js')({
+//         apiKey: api_key,
+//         domain: domain
+//     });
 
-    var mail = mailcomposer({
-        from: 'Do Not Reply <postmaster@sandbox47a06ea6aea54b5e9a408e3c2de6a8b7.mailgun.org>',
-        to: 'aaron.hill@me.com',
-        subject: 'Plane at Flying Dollar Airport',
-        text: 'A plane at Flying Dollar Airport! See the image at http://ml-runway.s3-website-us-east-1.amazonaws.com/' + foto,
-        html: '<h3>There is a plane at Flying Dollar Airport!</h3><p><img src="http://ml-runway.s3-website-us-east-1.amazonaws.com/' + foto + '"></img></p>'
-    });
+//     var mail = mailcomposer({
+//         from: 'Do Not Reply <postmaster@sandbox47a06ea6aea54b5e9a408e3c2de6a8b7.mailgun.org>',
+//         to: 'aaron.hill@me.com',
+//         subject: 'Plane at Flying Dollar Airport',
+//         text: 'A plane at Flying Dollar Airport! See the image at http://ml-runway.s3-website-us-east-1.amazonaws.com/' + foto,
+//         html: '<h3>There is a plane at Flying Dollar Airport!</h3><p><img src="http://ml-runway.s3-website-us-east-1.amazonaws.com/' + foto + '"></img></p>'
+//     });
 
-    mail.build(function(mailBuildError, message) {
+//     mail.build(function(mailBuildError, message) {
 
-        var dataToSend = {
-            to: 'aaron.hill@me.com',
-            message: message.toString('ascii')
-        };
+//         var dataToSend = {
+//             to: 'aaron.hill@me.com',
+//             message: message.toString('ascii')
+//         };
 
-        mailgun.messages().sendMime(dataToSend, function(sendError, body) {
-            if (sendError) {
-                winston.info(sendError);
-                return;
-            }
-        });
-    });
+//         mailgun.messages().sendMime(dataToSend, function(sendError, body) {
+//             if (sendError) {
+//                 winston.info(sendError);
+//                 return;
+//             }
+//         });
+//     });
 
-}
-
-function emailNoPlane(foto) {
-    var api_key = process.env.MAILGUN_KEY;
-    var domain = 'sandbox47a06ea6aea54b5e9a408e3c2de6a8b7.mailgun.org';
-    var mailgun = require('mailgun-js')({
-        apiKey: api_key,
-        domain: domain
-    });
-
-    var mail = mailcomposer({
-        from: 'Do Not Reply <postmaster@sandbox47a06ea6aea54b5e9a408e3c2de6a8b7.mailgun.org>',
-        to: 'aaron.hill@me.com',
-        subject: 'Runway Cam Activity',
-        text: 'The runway cam detected activity, but it is not an airplane. See the image at http://ml-runway.s3-website-us-east-1.amazonaws.com/' + foto,
-        html: '<h3>The runway cam detected activity, but it is not an airplane.</h3><p><img src="http://ml-runway.s3-website-us-east-1.amazonaws.com/' + foto + '"></img></p>'
-    });
-
-    mail.build(function(mailBuildError, message) {
-
-        var dataToSend = {
-            to: 'aaron.hill@me.com',
-            message: message.toString('ascii')
-        };
-
-        mailgun.messages().sendMime(dataToSend, function(sendError, body) {
-            if (sendError) {
-                winston.info(sendError);
-                return;
-            }
-        });
-    });
-}
+// }
 
 // TWILIO
-function sendMms (foto) {
-	var client = require('twilio')(process.env.TWILIO_ACCOUNTSID, process.env.TWILIO_AUTHTOKEN); 
-	var fotoUrl = "http://ml-runway.s3-website-us-east-1.amazonaws.com/" + foto; 
+// function sendMms (foto) {
+// 	var client = require('twilio')(process.env.TWILIO_ACCOUNTSID, process.env.TWILIO_AUTHTOKEN); 
+// 	var fotoUrl = "http://ml-runway.s3-website-us-east-1.amazonaws.com/" + foto; 
 	
-		for (var i=0; i<cellNumbers.length; i++) { 
-			client.messages.create({ 
-			    to: cellNumbers[i], 
-			    from: "+19177088720", 
-			    body: "Flying Dollar has a visitor!", 
-			    mediaUrl: fotoUrl,  
-			}, function(err, message) { 
-			    console.log(message.sid); 
-			});
-		}
-}
+// 		for (var i=0; i<cellNumbers.length; i++) { 
+// 			client.messages.create({ 
+// 			    to: cellNumbers[i], 
+// 			    from: "+19177088720", 
+// 			    body: "Flying Dollar has a visitor!", 
+// 			    mediaUrl: fotoUrl,  
+// 			}, function(err, message) { 
+// 			    console.log(message.sid); 
+// 			});
+// 		}
+// }
 
 // ITERATE
 
---
+++ communication.js
@@ -0,0 +1,105 @@
+var fs = require('fs');
+var et = fs.readFileSync('emailTemplate.txt').toString();
+var Mustache = require('mustache');
+
+module.exports = {
+
+emailPlane: function (foto, status) {
+    var emailContent = [];
+    var templateContent, emailSubject, output; 
+    var emailTo = 'runwaycam@mg.flyingdollar.email';
+
+    emailContent[0] = new Object();
+    emailContent[0].eHeader = 'Flying Dollar has a visitor!';
+    emailContent[0].eMessage = "The Runway Cam at Flying Dollar spotted a plane. We thought you'd like to know.";
+    emailContent[0].eButton = "Visit Flying Dollar";
+    emailContent[0].eUrl = "http://www.flyingdollar.com";
+    
+    emailContent[1] = new Object();
+    emailContent[1].eHeader = 'Unidentified flying object!';
+    emailContent[1].eMessage = "The Runway Cam detected an object that may or may not be a plane. If it is a plane, please let us know:";
+    emailContent[1].eButton = "It's a plane!";
+    // emailContent[1].eUrl = 'http://ml-runway.s3-website-us-east-1.amazonaws.com/' + foto;
+    emailContent[1].eUrl = "http://www.flyingdollar.com";
+    
+    emailContent[2] = new Object();
+    emailContent[2].eHeader = 'Squirrels, turkeys, bears, bugs, and tractors.';
+    emailContent[2].eMessage = "The Runway Cam detected an object that is unlikely to be a plane. But if it is a plane, please let us know:";
+    emailContent[2].eButton = "It's a plane!";
+    // emailContent[2].eUrl = 'http://ml-runway.s3-website-us-east-1.amazonaws.com/' + foto;
+    emailContent[2].eUrl = "http://www.flyingdollar.com";
+
+
+    if (status==false) {
+        templateContent = emailContent[2];
+        templateContent.eFoto = 'http://ml-runway.s3-website-us-east-1.amazonaws.com/' + foto; 
+        output = Mustache.render(et, templateContent);
+        emailSubject = 'Runway Cam Activity';
+        emailTo = 'aaron.hill@me.com'
+    }
+    
+    else if (status==true) {
+        templateContent = emailContent[0];
+        templateContent.eFoto = 'http://ml-runway.s3-website-us-east-1.amazonaws.com/' + foto; 
+        output = Mustache.render(et, templateContent);
+        emailSubject = 'Plane at Flying Dollar Airport';
+    }
+    
+    else if (status==null) {
+        templateContent = emailContent[1];
+        templateContent.eFoto = 'http://ml-runway.s3-website-us-east-1.amazonaws.com/' + foto; 
+        output = Mustache.render(et, templateContent);
+        emailSubject = 'Unidentified Flying Object at 8N4';
+    }
+
+    var mailcomposer = require('mailcomposer');
+    var api_key = process.env.MAILGUN_KEY;
+    var domain = 'mg.flyingdollar.email';
+    var mailgun = require('mailgun-js')({
+        apiKey: api_key,
+        domain: domain
+    });
+
+    var mail = mailcomposer({
+        from: 'Do Not Reply <postmaster@mg.flyingdollar.email>',
+        to: emailTo,
+        subject: emailSubject,
+        text: 'Runway Cam activity at Flying Dollar Airport! See the image at http://ml-runway.s3-website-us-east-1.amazonaws.com/' + foto,
+        html: output
+    });
+
+    mail.build(function(mailBuildError, message) {
+
+        var dataToSend = {
+            to: emailTo,
+            message: message.toString('ascii')
+        };
+
+        mailgun.messages().sendMime(dataToSend, function(sendError, body) {
+            if (sendError) {
+                console.log(sendError);
+                return;
+            }
+        });
+    });
+
+},
+
+// TWILIO
+sendMms: function (foto, cellNumbers) {
+	var client = require('twilio')(process.env.TWILIO_ACCOUNTSID, process.env.TWILIO_AUTHTOKEN); 
+	var fotoUrl = "http://ml-runway.s3-website-us-east-1.amazonaws.com/" + foto; 
+	
+		for (var i=0; i<cellNumbers.length; i++) { 
+			client.messages.create({ 
+			    to: cellNumbers[i], 
+			    from: "+19177088720", 
+			    body: "Flying Dollar has a visitor!", 
+			    mediaUrl: fotoUrl,  
+			}, function(err, message) { 
+			    console.log(message.sid); 
+			});
+		}
+}
+
+};
